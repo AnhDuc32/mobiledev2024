37d0f8328542d92ed03c7221f3d3fabfdc088140
AnhDuc32 <anhducnguyen3204@gmail.com>
2024-09-20 22:38:54 +0700

    Practice 11

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,6 +1,8 @@
 package vn.edu.usth.weather;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 
@@ -15,6 +17,12 @@ import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +36,32 @@ public class WeatherActivity extends AppCompatActivity {
         // Create and set TabLayout
         TabLayout tabLayout = findViewById(R.id.tabLayout);
         tabLayout.setupWithViewPager(viewPager);
+
+        // Extract the MP3 file
+        try {
+            InputStream inputStream = getResources().openRawResource(R.raw.on_and_on);
+            File outputFile = new File(getExternalFilesDir(Environment.DIRECTORY_MUSIC), "on_and_on.mp3");
+            OutputStream outputStream = new FileOutputStream(outputFile);
+
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = inputStream.read(buffer)) > 0) {
+                outputStream.write(buffer, 0, length);
+            }
+
+            inputStream.close();
+            outputStream.close();
+
+            // Initialize MediaPlayer
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            mediaPlayer.setDataSource(outputFile.getAbsolutePath());
+            mediaPlayer.prepare();
+            // Play the MP3 file
+            mediaPlayer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         Log.i("MyApp", "onCreate called"); // Log a message
     }
     @Override
