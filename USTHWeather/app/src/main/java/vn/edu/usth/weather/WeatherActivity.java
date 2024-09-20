934041643848934cf9b658cd78795c58feafe316
AnhDuc32 <anhducnguyen3204@gmail.com>
2024-09-20 23:52:04 +0700

    Practice 12

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,12 +1,18 @@
 package vn.edu.usth.weather;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -24,6 +30,8 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class WeatherActivity extends AppCompatActivity {
+    private MediaPlayer mediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,14 +61,23 @@ public class WeatherActivity extends AppCompatActivity {
             outputStream.close();
 
             // Initialize MediaPlayer
-            MediaPlayer mediaPlayer = new MediaPlayer();
+            mediaPlayer = new MediaPlayer();
             mediaPlayer.setDataSource(outputFile.getAbsolutePath());
-            mediaPlayer.prepare();
-            // Play the MP3 file
-            mediaPlayer.start();
+            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mediaPlayer) {
+                    mediaPlayer.start();
+                }
+            });
+            mediaPlayer.prepareAsync();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        // Set the ToolBar as the Action Bar
+        Toolbar toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
 
         Log.i("MyApp", "onCreate called"); // Log a message
     }
@@ -74,18 +91,55 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         // Resume animations, start media playback, etc.
+        if (mediaPlayer != null && !mediaPlayer.isPlaying()) {
+            mediaPlayer.start();
+        }
         Log.i("MyApp", "onResume called");
     }
     @Override
     protected void onPause() {
         super.onPause();
         // Save data or release resources
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+        }
         Log.i("MyApp", "onPause called");
     }
     @Override
     protected void onDestroy() {
         super.onDestroy();
         // Clean up resources, close connections, etc.
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
         Log.i("MyApp", "onDestroy called");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        // Handle the refresh action
+        if (id == R.id.refresh) {
+            refreshContent();
+            return true;
+        }
+        // Handle the settings action
+        if (id == R.id.settings) {
+            Intent intent = new Intent(this, PrefActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void refreshContent() {
+        Toast.makeText(this, "Refreshing", Toast.LENGTH_SHORT).show();
     }
 }
